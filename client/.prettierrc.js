ee660f57b99a53a945bbbd44ca76da2b5aa40f52
Egoe Dmitriev <egas099@gmail.com>
2022-02-12 10:05:11 +0300

    install prettier and eslint

---
+++ client/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    singleQuote: true,
+    printWidth: 80,
+    tabWidth: 4,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    semi: false,
+    useTabs: false,
+    jsxBracketSameLine: false,
+    arrowParens: 'avoid'
+}
